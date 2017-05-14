a928ef0310f0fc223aa7ecff95960eab57889559
Dennis.westphal <mail@denniswestphal.de>
2017-05-14 18:31:13 +1000

    Clean up files

--- 3-conference.sql
+++ 3-conference.sql
@@ -17,18 +17,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 CREATE SCHEMA IF NOT EXISTS `cakephp` DEFAULT CHARACTER SET utf8 ;
 USE `cakephp` ;
 
--- -----------------------------------------------------
--- Table `cakephp`.`cars`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cakephp`.`cars` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `owner` VARCHAR(255) NOT NULL,
-  `model` VARCHAR(255) NOT NULL,
-  `horsePower` SMALLINT UNSIGNED NOT NULL,
-  `color` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `cakephp`.`users`
@@ -109,15 +97,6 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `cakephp`.`cars`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `cakephp`;
-INSERT INTO `cakephp`.`cars` (`id`, `owner`, `model`, `horsePower`, `color`) VALUES (1, 'Dennis Westphal', 'Tesla Model S', 463, 'white');
-
-COMMIT;
 
 
 -- -----------------------------------------------------
