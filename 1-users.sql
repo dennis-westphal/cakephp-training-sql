49f650f6b3bf5f30175d8b6225ba421439d0eca7
Dennis.westphal <mail@denniswestphal.de>
2017-05-14 18:26:29 +1000

    Prefixed for run order

---
+++ 1-users.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA `cars`;
+CREATE SCHEMA `my_app`;
+CREATE SCHEMA `cakephp`;
+
+CREATE USER `cars`@`localhost`;
+CREATE USER `my_app`@`localhost` IDENTIFIED BY "secret";
+CREATE USER `cakephp`@`localhost`;
+
+GRANT ALL ON `cars`.* TO `cars`@`localhost`;
+GRANT ALL ON `my_app`.* TO `my_app`@`localhost`;
+GRANT ALL ON `cakephp`.* TO `cakephp`@`localhost`;
